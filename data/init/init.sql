497808154ed67f361988f02652b67332f3ce427e
Rokli02 <marko.roland.2000@gmail.com>
2025-01-25 01:35:42 +0100

    db restricted naming fix + add unknown user

--- data/init/init.sql
+++ data/init/init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE User (
+CREATE TABLE user (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE User (
     rights INT
 );
 
-CREATE TABLE Product (
+CREATE TABLE product (
      id BIGINT AUTO_INCREMENT PRIMARY KEY,
      name VARCHAR(255) NOT NULL,
      description VARCHAR(255) NOT NULL,
@@ -19,15 +19,17 @@ CREATE TABLE Product (
      is_active BOOLEAN DEFAULT TRUE
 );
 
-CREATE TABLE Order (
+CREATE TABLE product_order (
      id BIGINT AUTO_INCREMENT PRIMARY KEY,
      product_id BIGINT NOT NULL,
-     user_id BIGINT NOT NULL,
+     user_id BIGINT NOT NULL DEFAULT 0,
      quantity INT NOT NULL,
      price INT NOT NULL,
-     group VARCHAR(255),
+     o_group VARCHAR(255),
      status INT NOT NULL,
      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-     FOREIGN KEY (product_id) REFERENCES Product(id),
-     FOREIGN KEY (user_id) REFERENCES User(id)
+     FOREIGN KEY (product_id) REFERENCES product(id),
+     FOREIGN KEY (user_id) REFERENCES user(id)
 );
+
+INSERT INTO user (username, password, name) VALUES('unknown', '#!%~¬§', "Unknown");
